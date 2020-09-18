0097cd7019aed95c6bb9a66a29c61eabb3441297
msherry14 <71249924+msherry14@users.noreply.github.com>
2020-09-19 01:13:27 +0300

    bigger piece of sh... norm alg

--- src/norm_algo.py
+++ src/norm_algo.py
@@ -1,16 +1,25 @@
 # read a way to file
-file_way = "D:/test/test1.txt"
-# read substitution set
+file_way = "D:/test/test1.txt" #test block
+# read substitutions and words
 substitutions = []
+words = []
 with open(file_way, "r") as file:
-    n = int(readline())
-    for i in range(n):
-        substitutions.append(readline())
-    k = int(readline())
-    for i in range(k):
-        words.append(readline())
-print(substitutions, sep="\n")
-print(words, sep="\n")
-
-# turn into
-# s = s.split(" ")
+    sub_count = int(file.readline())
+    for i in range(sub_count):
+        substitutions.append(file.readline())
+    word_count = int(file.readline())
+    for i in range(word_count):
+        words.append(file.readline())
+#bring a word and do this
+while i < word_count:
+    word = words[i]
+    while k < sub_count:
+        sub = substitutions[k]
+        s = sub.split(" ")
+        if word.find(s[0]) != -1:
+            word.replace(s[0], s[1], 1)
+            words[i] = word
+            # breaking if 1 in s[2] else j = 0
+        else
+            k += 1
+    i += 1
